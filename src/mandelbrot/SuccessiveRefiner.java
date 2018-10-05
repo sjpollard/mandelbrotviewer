59d9b76208c3f6142a2a3dbebfc87deff4d00b40
Samuel J Pollard <40839788+sjpollard@users.noreply.github.com>
2018-10-05 21:19:50 +0100

    SuccessiveRefiner annotations

--- src/mandelbrot/SuccessiveRefiner.java
+++ src/mandelbrot/SuccessiveRefiner.java
@@ -1,15 +1,28 @@
 package mandelbrot;
 
+/**
+ * Custom runnable that can be used to create a thread that allows for this program to iterate using successive
+ * refinement. This class calls for iteration at increasing resolutions and displays after each one, allowing for
+ * the user to see a developing image of increasing effective resolution. The Thread.sleep() method is used here
+ * to allow for the user to be able to actually see the image when it has been created, otherwise only the final
+ * image is displayed.
+ */
+
 public class SuccessiveRefiner implements Runnable{
 
+    /**Reference to the GUI*/
     private MandelbrotFrame mandelbrotFrame;
+
+    /**Chunk size at which to stop successive refinement*/
     private int breakPoint;
 
-    public SuccessiveRefiner(MandelbrotFrame GUI) {
-        this.mandelbrotFrame = GUI;
-        this.breakPoint = GUI.mandelbrotSet.getChunkSize();
+    /**Constructor that sets the fields to the inputs*/
+    public SuccessiveRefiner(MandelbrotFrame mandelbrotFrame) {
+        this.mandelbrotFrame = mandelbrotFrame;
+        this.breakPoint = mandelbrotFrame.mandelbrotSet.getChunkSize();
     }
 
+    /**Method that is called when a thread is created based on this runnable which starts the successive refinement process*/
     @Override
     public void run() {
 
