e26a3bafffdca088fb52ca587ac13736660b2a5e
Samuel J Pollard <40839788+sjpollard@users.noreply.github.com>
2018-10-11 20:17:26 +0100

    Improved leniency of RegexManager

--- src/mandelbrot/RegexManager.java
+++ src/mandelbrot/RegexManager.java
@@ -9,21 +9,37 @@ package mandelbrot;
 public class RegexManager {
 
     /**Regex constants that can be matched with an input string*/
-    private static final String validInteger = "-?[0-9]+";
-    private static final String validDouble = validInteger + "\\.[0-9]+";
-    private static final String validComplexNumber = validDouble + "(\\+|-)[0-9]+\\.[0-9]+i";
+    private static final String validUnsignedInteger = "[0-9]+";
+    private static final String validSignedInteger = "[+-]?" + validUnsignedInteger;
+    private static final String validUnsignedDouble = validUnsignedInteger + "(\\.[0-9]+)?";
+    private static final String validSignedDouble = "[+-]?" + validUnsignedDouble;
+    private static final String validComplexNumber = "(" + validSignedDouble + "[+-]" + validUnsignedInteger + "i \\|" + validSignedDouble + "\\|" + validSignedDouble+ "i)";
+
+    /**Decided whether the string is a valid positive integer*/
+    public static boolean matchesUnsignedInteger(String input) {
+
+        return input.matches(validUnsignedInteger);
+
+    }
 
     /**Decides whether the string is a valid integer*/
-    public static boolean matchesInteger(String input) {
+    public static boolean matchesSignedInteger(String input) {
 
-        return input.matches(validInteger);
+        return input.matches(validSignedInteger);
 
     }
 
     /**Decides whether the string is a valid double*/
-    public static boolean matchesDouble(String input) {
+    public static boolean matchesUnsignedDouble(String input) {
 
-        return input.matches(validDouble);
+        return input.matches(validUnsignedDouble);
+
+    }
+
+    /**Decides whether the string is a valid double*/
+    public static boolean matchesSignedDouble(String input) {
+
+        return input.matches(validSignedDouble);
 
     }
 
