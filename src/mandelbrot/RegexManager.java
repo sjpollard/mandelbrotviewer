0c72d5fed94255e23c144c30ac70f51158e3ce5b
Samuel Joseph Pollard <40839788+sjpollard@users.noreply.github.com>
2018-10-17 16:05:25 +0100

    Oops

--- src/mandelbrot/RegexManager.java
+++ src/mandelbrot/RegexManager.java
@@ -1,14 +1,14 @@
 package mandelbrot;
 
 /**
- * Static class that can be used to ate whether a string is an integer, a double or even
- * a ComplexNumber (in the form a+bi). This is used within SettingsFrame to ate whether
+ * Static class that can be used to validate whether a string is an integer, a double or even
+ * a ComplexNumber (in the form a+bi). This is used within SettingsFrame to validate whether
  * the values that have been entered are suitable.
  */
 
 public class RegexManager {
 
-   /**Regex matching with a positive integer*/
+    /**Regex matching with a positive integer*/
     private static final String unsignedInteger = "[0-9]+";
     /**Regex matching with a signed integer*/
     private static final String signedInteger = "[+-]?" + unsignedInteger;
@@ -19,35 +19,35 @@ public class RegexManager {
     /**Regex matching with a complex number*/
     private static final String complexNumber = "(" + signedDouble + "[+-]" + unsignedDouble + "i|" +  signedDouble + "i?)";
 
-    /**Decided whether the string is a  positive integer*/
+    /**Decided whether the string is a valid positive integer*/
     public static boolean matchesUnsignedInteger(String input) {
 
         return input.matches(unsignedInteger);
 
     }
 
-    /**Decides whether the string is a  integer*/
+    /**Decides whether the string is a valid integer*/
     public static boolean matchesSignedInteger(String input) {
 
         return input.matches(signedInteger);
 
     }
 
-    /**Decides whether the string is a  positive double*/
+    /**Decides whether the string is a valid positive double*/
     public static boolean matchesUnsignedDouble(String input) {
 
         return input.matches(unsignedDouble);
 
     }
 
-    /**Decides whether the string is a  double*/
+    /**Decides whether the string is a valid double*/
     public static boolean matchesSignedDouble(String input) {
 
         return input.matches(signedDouble);
 
     }
 
-    /**Decides whether the string is a  complex number*/
+    /**Decides whether the string is a valid complex number*/
     public static boolean matchesComplexNumber(String input) {
 
         return input.matches(complexNumber);
