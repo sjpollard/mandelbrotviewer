115bafd43d61f0f8b0272dd750db4e84521f26e7
Samuel Joseph Pollard <40839788+sjpollard@users.noreply.github.com>
2018-10-18 12:55:21 +0100

    Renaming of DoublyLinked

--- src/mandelbrot/GenericDataStructure.java
+++ src/mandelbrot/GenericDataStructure.java
@@ -1,13 +1,13 @@
 package mandelbrot;
 
 /**
- * Interface used by the DoublyLinked data structures to ensure they contain the proper methods
+ * Interface used by the generic data structures to ensure they contain the proper methods
  * that are necessary for a dynamic data structure to function. As the inner workings of the data
  * structures are different, they implement them in different ways.
  *@param <T>
  */
 
-public interface DoublyLinked<T> {
+public interface GenericDataStructure<T> {
 
     /**Adds T to the data structure in the correct place*/
     void add(T item);
